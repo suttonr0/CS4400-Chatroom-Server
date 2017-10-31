985069f7b2fd216c74a828c3fbd51c6a397204c8
Rowan Christopher Sutton <suttonr@lg12l35.scss.tcd.ie>
2017-10-31 15:36:48 +0000

    made functions for two types of client command

--- CS4400 Chat Server Multithread.py
+++ CS4400 Chat Server Multithread.py
@@ -20,11 +20,28 @@ print('Server Setup Complete')
 # # hence why there is no listen method used
 
 # HELO function
+def heloFunction(inputMessage, conn):
+    print("Received HELO")
+    localAddressIP = gethostbyname(getfqdn())  # RETURNS INCORRECT IP ADDRESS FOR EXTERNAL USE
+    helo_reply = "HELO{}IP:{}\nPort:{}\nStudentID:{}\n".format(inputMessage[4:],localAddressIP,serverPort,studentID)     
+    conn.send(helo_reply.encode())
+    print("Sent Reply {}".format(helo_reply))
 
 
 # Join chatroom function
+def joinChatroom(inputMessage, conn):
+    # INCLUDE CODE TO JOIN A CHATROOM (ADD TO A CHATROOM LIST VIA MUTEX)
+    join_text_split = inputMessage.splitlines()  # splits message by newlines
+    join_room_name = join_text_split[0][15:]  # First line with "JOIN_CHATROOM: " cut off
+    join_client_ip = join_text_split[1][11:]  # Second line with "CLIENT_IP: " cut off
+    join_port = join_text_split[2][6:]  # Third line with "PORT: " cut off
+    join_client_name = join_text_split[0][13:]  # Fourth line with "CLIENT_NAME: " cut off
 
-
+    room_number = 0  # replace with correct room numbering system
+    client_number = 0  # replace with correct client numbering system
+    join_text_response = "JOINED_CHATROOM: {}\nSERVER_IP: {}\nPORT: {}\nROOM_REF: {}\nJOIN_ID: {}\n".format(join_room_name, gethostbyname(getfqdn()), join_port, room_number, client_number)
+    print(join_text_response.encode())
+    conn.send(join_text_response.encode())
 
 
 
@@ -42,25 +59,11 @@ def receive_clients(conn):
 	# HANDLING INPUT
 	
         if receivedMessage[:4] == "HELO":  # Check if first 4 chars are HELO
-            print("Received HELO")
-            localAddressIP = gethostbyname(getfqdn())  # RETURNS INCORRECT IP ADDRESS FOR EXTERNAL USE
-            helo_reply = "HELO{}IP:{}\nPort:{}\nStudentID:{}\n".format(receivedMessage[4:],localAddressIP,serverPort,studentID)     
-            conn.send(helo_reply.encode())
-            print("Sent Reply {}".format(helo_reply))
+            heloFunction(receivedMessage, conn)
 
         elif receivedMessage[:13] == "JOIN_CHATROOM":
-            # INCLUDE CODE TO JOIN A CHATROOM (ADD TO A CHATROOM LIST VIA MUTEX)
-            join_text_split = receivedMessage.splitlines()  # splits message by newlines
-            join_room_name = join_text_split[0][15:]  # First line with "JOIN_CHATROOM: " cut off
-            join_client_ip = join_text_split[1][11:]  # Second line with "CLIENT_IP: " cut off
-            join_port = join_text_split[2][6:]  # Third line with "PORT: " cut off
-            join_client_name = join_text_split[0][13:]  # Fourth line with "CLIENT_NAME: " cut off
+            joinChatroom(receivedMessage, conn)
 
-            room_number = 0  # replace with correct room numbering system
-            client_number = 0  # replace with correct client numbering system
-            join_text_response = "JOINED_CHATROOM: {}\nSERVER_IP: {}\nPORT: {}\nROOM_REF: {}\nJOIN_ID: {}\n".format(join_room_name, gethostbyname(getfqdn()), join_port, room_number, client_number)
-            print(join_text_response.encode())
-            conn.send(join_text_response.encode())
         elif receivedMessage == "KILL_SERVICE\n":  # When telnet leaves, it sends blank data. Replace with end message
             break
         #else:
